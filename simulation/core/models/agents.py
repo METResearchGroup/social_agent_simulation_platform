8fc7afbdd1214319ebf417cc6c8e65eab0b93321
mark-torres10 <markptorres1@gmail.com>
2026-02-13 18:23:58 -0600

    add no-op for cases of no actions, which we want to address later

--- simulation/core/models/agents.py
+++ simulation/core/models/agents.py
@@ -40,10 +40,18 @@ class SocialMediaAgent:
         )
 
     def like_posts(self, feed: list[BlueskyFeedPost]) -> list[GeneratedLike]:
+        if len(feed) == 0:
+            print("[No-op for now] No posts to like.")
+            return []
         return []
 
     def comment_posts(self, feed: list[BlueskyFeedPost]) -> list[GeneratedComment]:
+        if len(feed) == 0:
+            print("[No-op for now] No posts to comment on.")
+            return []
         return []
 
     def follow_users(self, feed: list[BlueskyFeedPost]) -> list[GeneratedFollow]:
+        if len(feed) == 0:
+            print("[No-op for now] No users to follow.")
         return []
