6ea5713f8f1089ac5669757d8134813f0eb9aa82
mark-torres10 <markptorres1@gmail.com>
2026-02-23 19:42:03 -0600

    Extract algorithm config block

--- ui/components/details/RunParametersBlock.tsx
+++ ui/components/details/RunParametersBlock.tsx
@@ -6,6 +6,29 @@ interface RunParametersBlockProps {
   config: RunConfig | null;
 }
 
+interface AlgorithmConfigBlockProps {
+  entries: [string, unknown][];
+}
+
+function AlgorithmConfigBlock({ entries }: AlgorithmConfigBlockProps) {
+  if (entries.length === 0) {
+    return null;
+  }
+
+  return (
+    <div className="pt-1">
+      <div className="text-beige-900">Algorithm Config:</div>
+      <div className="pl-3">
+        {entries.map(([key, value]) => (
+          <div key={key}>
+            {key}: {typeof value === 'string' ? value : JSON.stringify(value)}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
 export default function RunParametersBlock({ config }: RunParametersBlockProps) {
   if (!config) {
     return null;
@@ -22,18 +45,7 @@ export default function RunParametersBlock({ config }: RunParametersBlockProps) 
         <div>Agents: {config.numAgents}</div>
         <div>Turns: {config.numTurns}</div>
         <div>Algorithm: {config.feedAlgorithm}</div>
-        {configEntries.length > 0 && (
-          <div className="pt-1">
-            <div className="text-beige-900">Algorithm Config:</div>
-            <div className="pl-3">
-              {configEntries.map(([key, value]) => (
-                <div key={key}>
-                  {key}: {typeof value === 'string' ? value : JSON.stringify(value)}
-                </div>
-              ))}
-            </div>
-          </div>
-        )}
+        <AlgorithmConfigBlock entries={configEntries} />
       </div>
     </div>
   );
