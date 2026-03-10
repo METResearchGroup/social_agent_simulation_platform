ea210f29c2649d7496ba58b144827fac04c6179b
dudu-theman <dustanley07@gmail.com>
2026-03-10 15:12:21 -0500

    implemented NERModel

---
+++ ml_tooling/ner/model.py
@@ -0,0 +1,45 @@
+from pydantic import BaseModel
+from transformers import pipeline
+
+from lib.timestamp_utils import get_current_timestamp
+
+
+class EntitySpan(BaseModel):
+    text: str  # (surface form)
+    label: str  # (e.g. PER, ORG, LOC, MISC, or model-provided label)
+    score: float
+    timestamp: str  # (use the get_current_timestamp in lib/timestamp_utils.py to calculate this)
+
+
+class NERModel:
+    def __init__(self):
+        self.ner = pipeline(
+            "token-classification",
+            model="dslim/bert-base-NER",
+            aggregation_strategy="simple",
+        )
+
+    def extract_entities(self, text: str) -> list[EntitySpan]:
+        response = self.ner(text)
+
+        lo_entity_span = []
+        for entity in response:
+            text = entity["word"]
+            label = entity["entity_group"]
+            score = float(entity["score"])
+            timestamp = get_current_timestamp()
+
+            lo_entity_span.append(
+                EntitySpan(text=text, label=label, score=score, timestamp=timestamp)
+            )
+
+        return lo_entity_span
+
+    def extract_entities_batch(self, texts: list[str]) -> list[list[EntitySpan]]:
+
+        lolo_entity_span = []
+        for text in texts:
+            lo_entity_span = self.extract_entities(text)
+            lolo_entity_span.append(lo_entity_span)
+
+        return lolo_entity_span
